e50a02a86d7a904f019b6a9728081eab6b2e8c08
Eric <ericgmicaela@gmail.com>
2020-05-14 17:33:30 -0300

    Desafio 29 final

--- Desafio028.py
+++ Desafio028.py
@@ -1,1 +1,10 @@
-print(f"{'*'*10}Desafio 028 - Descobrir número Random{'*'*10}")
+import math, random
+
+print(f"{'*' * 10}Desafio 028 - Descobrir número Random{'*' * 10}")
+escolha_pc = random.randrange(1, 5)
+escolha_user = int(input("Informe o numero para comparar com o Computador: "))
+
+if escolha_pc == escolha_user:
+    print("Parabéns Você acertou!!")
+else:
+    print(f"Lamento! Tente novamente. escolha do pc: {escolha_pc}")
---
+++ Desafio029.py
@@ -0,0 +1,7 @@
+print(f"{'*'*10}Desafio 029 - Compara se foi multado{'*'*10}")
+valor_multa = float(input("Digite o valor da multa por km vigente: "))
+velocidade_limite = int(input("Informe o limite de velocidade em km/h: "))
+velocidade_atual = int(input("Informe a velocidade atual: "))
+velocidade_acima = velocidade_atual-velocidade_limite
+
+print(f"Você ultrapassou o limite da via em {velocidade_acima}km/h e foi multado em R${velocidade_acima*valor_multa}" if velocidade_acima > 0 else f"Velocidade: {velocidade_atual}km/h")
