08ab055a692b2d50a4176012d410657ba37bfe03
Eric <ericgmicaela@gmail.com>
2020-05-14 16:44:20 -0300

    Versão do PC 14/05 16:50

--- Aula10.py
+++ Aula10.py
@@ -1,3 +1,15 @@
-#Testando
-print("BOLT PARA FORA")
-#editado pelo GIT
+tempo = int(input("Digite a idade do seu carro: "))
+
+## Maneira Usual:
+if tempo <= 3:
+    print ("Carro Novo")
+else:
+    print ("Carro Velho")
+
+print("Fim!")
+
+## Maneira rápida:
+print('Carro Novo' if tempo <=3 else 'Carro Velho')
+
+
+
