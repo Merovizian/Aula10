a229198b35b7a2e7406e39b9255b93a717096754
Merovizian <48018611+Merovizian@users.noreply.github.com>
2020-05-14 21:37:30 -0300

    Git 14/05 - 21:37

--- Desafio029.py
+++ Desafio029.py
@@ -5,3 +5,4 @@ velocidade_atual = int(input("Informe a velocidade atual: "))
 velocidade_acima = velocidade_atual-velocidade_limite
 
 print(f"Você ultrapassou o limite da via em {velocidade_acima}km/h e foi multado em R${velocidade_acima*valor_multa}" if velocidade_acima > 0 else f"Velocidade: {velocidade_atual}km/h")
+#Editado pelo git
